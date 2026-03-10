c3ef64826f477fe6903dae1729edc3597a8cc499
PabloDanielMC27 <152151353+PabloDanielMC27@users.noreply.github.com>
2026-03-10 00:13:16 -0400

    Implement dynamic programming solution for min path sum

---
+++ leetcode_64.py
@@ -0,0 +1,59 @@
+# 64. Minimum Path Sum
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+
+        # def backtrack(m, n):
+
+        #     if m == 0 and n == 0:
+        #         return grid[0][0]
+
+        #     if m < 0 or n < 0:
+        #         return float("inf")
+
+        #     return grid[m][n] + min(backtrack(m - 1, n), backtrack(m, n - 1)) 
+
+        # m = len(grid)
+        # n = len(grid[0])
+        # return backtrack(m - 1, n - 1)
+
+        # def backtrack2(m, n, dp):
+
+        #     if m == 0 and n == 0:
+        #         return grid[0][0]
+
+        #     if m < 0 or n < 0:
+        #         return float("inf")
+            
+        #     if dp[m][n] != -1:
+        #         return dp[m][n]
+
+        #     dp[m][n] = grid[m][n] + min(backtrack2(m - 1, n, dp), backtrack2(m, n - 1, dp)) 
+        #     return dp[m][n]
+
+        # m = len(grid)
+        # n = len(grid[0])
+        # dp = [[-1] * n for _ in range(m)]
+        # dp[0][0] = grid[0][0]
+        # return backtrack2(m - 1, n - 1, dp)
+
+        def backtrack3(m, n):
+
+            dp = [[-1] * n for _ in range(m)]
+            dp[0][0] = grid[0][0]
+
+            for row in range(1, m):
+                dp[row][0] = grid[row][0] + dp[row - 1][0]
+
+            for col in range(1, n):
+                dp[0][col] = grid[0][col] + dp[0][col - 1]
+
+            for row in range(1, m):
+                for col in range(1, n):
+                    dp[row][col] = grid[row][col] + min(dp[row - 1][col], dp[row][col -1])
+ 
+            return dp[m - 1][n - 1]
+
+        m = len(grid)
+        n = len(grid[0])
+        return backtrack3(m, n)        
