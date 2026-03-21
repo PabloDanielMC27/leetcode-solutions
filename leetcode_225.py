f001963a1ef42a44d2af4cb16aaa6ae5fa57d47f
PabloDanielMC27 <152151353+PabloDanielMC27@users.noreply.github.com>
2026-03-21 14:07:30 -0400

    Add MyStack implementation using single queue
    Implement a stack using a single queue with push, pop, top, and empty methods.

---
+++ leetcode_225.py
@@ -0,0 +1,29 @@
+# 225. Implement Stack using Queues
+
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+        for _ in range(len(self.q1) - 1):
+            popFront = self.q1.popleft()
+            self.q1.append(popFront)
+
+    def pop(self) -> int:
+        return self.q1.popleft()
+
+    def top(self) -> int:
+        return self.q1[0]
+
+    def empty(self) -> bool:
+        return not self.q1
+        
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
