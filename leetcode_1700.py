cf604e3df6978747452957880f032829503d51f3
PabloDanielMC27 <152151353+PabloDanielMC27@users.noreply.github.com>
2026-04-06 13:04:57 -0400

    Refactor countStudents to use Counter for sandwiches
    Refactor countStudents method to use counting instead of queue.

--- leetcode_1700.py
+++ leetcode_1700.py
@@ -3,26 +3,39 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
 
-        n = len(students)
-        stu_queue = deque(students)
-        queue = deque(sandwiches)
         
-        count = 0
-        while True:
-            if queue[0] == stu_queue[0]:
-                #valid
-                queue.popleft()
-                stu_queue.popleft()
-                n -= 1
-                count = 0
+        # n = len(students)
+        # stu_queue = deque(students)
+        # queue = deque(sandwiches)
+        
+        # count = 0
+        # while True:
+        #     if queue[0] == stu_queue[0]:
+        #         #valid
+        #         queue.popleft()
+        #         stu_queue.popleft()
+        #         n -= 1
+        #         count = 0
 
+        #     else:
+        #         student = stu_queue.popleft()
+        #         stu_queue.append(student)
+        #         count += 1
+
+        #     if count == n:
+        #         return n
+
+
+        # counting
+        count = Counter(students)
+
+        for sandwich in sandwiches:
+            if count[sandwich] > 0:
+                count[sandwich] -= 1
             else:
-                student = stu_queue.popleft()
-                stu_queue.append(student)
-                count += 1
-
-            if count == n:
-                return n
+                break
+        
+        return count[0] + count[1]
             
 
         
