b5c77defeb12248a1e34022afd30d28af9e61741
PabloDanielMC27 <152151353+PabloDanielMC27@users.noreply.github.com>
2026-04-07 07:53:18 -0400

    Implement rectangle construction based on area

---
+++ leetcode_492.py
@@ -0,0 +1,11 @@
+# 492. Construct the Rectangle
+
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+
+        width = int(math.sqrt(area))
+
+        while area % width != 0:
+            width -= 1
+        
+        return [area // width, width]
