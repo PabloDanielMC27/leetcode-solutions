00c4c2119dc2bed5871cec39dc644acfba0b78d6
Pablo Molina <152151353+PabloDanielMC27@users.noreply.github.com>
2026-07-23 09:34:00 -0400

    Add NumArray class for range sum query
    Implement NumArray class for range sum queries.

---
+++ leetcode_303.py
@@ -0,0 +1,16 @@
+# 303. Range Sum Query - Immutable
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = [0]
+
+        for i in range(len(nums)):
+            self.nums.append(nums[i] + self.nums[i])   
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.nums[right + 1] - self.nums[left]
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
