2eda15ecdbca7cb627fef0b012f2044ba26b4f99
Pablo Molina <152151353+PabloDanielMC27@users.noreply.github.com>
2026-07-23 22:50:05 -0400

    Add BFS solution for finding target node in cloned tree
    Implement BFS to find corresponding node in cloned tree.

---
+++ leetcode_1379.py
@@ -0,0 +1,45 @@
+# 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+
+        # dfs
+        # def dfs(root):
+        #     if not root:
+        #         return None
+
+        #     lh = dfs(root.left)
+        #     if lh:
+        #         return lh
+        #     rh = dfs(root.right)
+        #     if rh:
+        #         return rh
+
+        #     if root.val == target.val:
+        #         return root
+ 
+        # return dfs(cloned)
+        
+        # bfs
+        queue = deque()
+        queue.append(cloned)
+
+        while queue:
+            node = queue.popleft()
+
+            if node.val == target.val:
+                return node
+            
+            if node.left:
+                queue.append(node.left)
+            
+            if node.right:
+                queue.append(node.right)
+
